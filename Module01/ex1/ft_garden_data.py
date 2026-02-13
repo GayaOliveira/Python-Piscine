dc62dbe3ff7a634adb462437f76db9922afc5f6a
GayaOliveira <gmo01233210@gmail.com>
2026-02-13 17:06:45 -0300

    fix: modify ex1 (Plant class changed)

--- Module01/ex1/ft_garden_data.py
+++ Module01/ex1/ft_garden_data.py
@@ -1,22 +1,28 @@
 class Plant:
-    def __init__(self, name, height, age):
-        self.name = name
-        self.height = height
-        self.age = age
-
-    def __str__(self):
-        return f"{self.name}: {self.height}cm, {self.age} days old"
+    def __init__(self):
+        self.name = ""
+        self.height = 0
+        self.age = 0
     
 
 plants = []
 
-p1 = Plant("Rose", 25, 30)
-p2 = Plant("Sunflower", 80, 45)
-p3 = Plant("Cactus", 15, 120)
+p1 = Plant()
+p1.name = "Rose"
+p1.height = 25
+p1.age = 30
+p2 = Plant()
+p2.name = "Sunflower"
+p2.height = 80
+p2.age = 45
+p3 = Plant()
+p3.name = "Cactus"
+p3.height = 15
+p3.age = 120
 
 plants.append(p1)
 plants.append(p2)
 plants.append(p3)
 
 for i in range (0, len(plants)):
-    print(plants[i])
+    print(f"{plants[i].name}: {plants[i].height}cm, {plants[i].age} days old")
