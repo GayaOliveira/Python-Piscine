44fb36e78a651b168bf076f1d5d9559585f130ac
GayaOliveira <gmo01233210@gmail.com>
2026-02-19 13:09:47 -0300

    fix: change encapsulation in class Plant in ex4

--- Module01/ex4/ft_garden_security.py
+++ Module01/ex4/ft_garden_security.py
@@ -1,56 +1,52 @@
-class Plant:
+class SecurePlant:
     def __init__(self, name: str, height: int, age: int) -> None:
-        self.name = name
-        self.height = height
-        self.age = age
+        self.__name = name
+        self.__height = height
+        self.__age = age
 
-    @property
-    def name(self) -> str:
-        return self._name
+    def get_name(self) -> str:
+        return self.__name
 
-    @name.setter
-    def name(self, new_name: int) -> None:
-        self._name = new_name
+    def get_height(self) -> int:
+        return self.__height
+    
+    def get_age(self) -> int:
+        return self.__age
 
-    @property
-    def height(self) -> int:
-        return self._height
+    def set_name(self, new_name: int) -> None:
+        self.__name = new_name
 
-    @height.setter
-    def height(self, new_height: int) -> None:
+    def set_height(self, new_height: int) -> None:
         if new_height < 0:
             print(f"Invalid operation attempted: height {new_height}cm [REJECTED]")
             print(f"Security: Negative height rejected")
             print()
-            print(f"Current plant: Rose ({self.height}cm, {self.age} days)")
+            print(f"Current plant: Rose ({self.get_height()}cm, {self.get_age()} days)")
         else:
-            self._height = new_height
+            self.__height = new_height
 
-    @property
-    def age(self) -> int:
-        return self._age
-
-    @age.setter
-    def age(self, new_age: int) -> None:
+    
+    def set_age(self, new_age: int) -> None:
         if new_age < 0:
             print(f"Invalid operation attempted: age {new_age} days [REJECTED]")
             print(f"Security: Negative age rejected")
             print()
-            print(f"Current plant: Rose ({self.height}cm, {self.age} days)")
+            print(f"Current plant: Rose ({self.get_height()}cm, {self.get_age()} days)")
         else:
-            self._age = new_age
-        
+            self.__age = new_age        
+
     def __str__(self) -> str:
-        return f"Plant created: {self.name}\nHeight updated: {self.height}cm [OK]\nAge updated: {self.age} days [OK]"
+        return f"Plant created: {self.__name}\nHeight updated: {self.__height}cm [OK]\nAge updated: {self.__age} days [OK]"
     
 
-rose = Plant("Rose", 25, 30)
-print("=== Garden Security System ===")
-print(rose)
-print()
-rose.height = -5
-print()
-rose.age = -12
-print()
-rose.height = 26
-print(rose)
+if __name__ == "__main__":
+    rose = SecurePlant("Rose", 25, 30)
+    print("=== Garden Security System ===")
+    print(rose)
+    print()
+    rose.set_height(-5)
+    print()
+    rose.set_age(-12)
+    print()
+    rose.set_height(26)
+    print(rose)
