2f8f0dd47bbf20e4096f5f3575748a1246d4c7fa
GayaOliveira <gmo01233210@gmail.com>
2026-02-19 12:29:10 -0300

    fix: improve main and remove blank lines in class Plant in ex2

--- Module01/ex2/ft_plant_growth.py
+++ Module01/ex2/ft_plant_growth.py
@@ -4,19 +4,15 @@ class Plant:
         self.height = 0
         self.age = 0
 
-
     def grow_higher(self, cm: int) -> None:
         self.height = self.height + cm
-
 
     def grow_older(self, days: int) -> None:
         self.age = self.age + days
 
-
     def grow_a_week(self) -> None:
         self.grow_higher(6)
         self.grow_older(6)
-
 
     def get_info(self) -> str:
         info = f"""=== Day 1 ===
@@ -28,25 +24,22 @@ class Plant:
         return info
 
 
-plants = []
+if __name__ == "__main__":
+    p1 = Plant()
+    p1.name = "Rose"
+    p1.height = 25
+    p1.age = 30
+    p2 = Plant()
+    p2.name = "Sunflower"
+    p2.height = 80
+    p2.age = 45
+    p3 = Plant()
+    p3.name = "Cactus"
+    p3.height = 15
+    p3.age = 120
 
-p1 = Plant()
-p1.name = "Rose"
-p1.height = 25
-p1.age = 30
-p2 = Plant()
-p2.name = "Sunflower"
-p2.height = 80
-p2.age = 45
-p3 = Plant()
-p3.name = "Cactus"
-p3.height = 15
-p3.age = 120
+    plants = [p1, p2, p3]
 
-plants.append(p1)
-plants.append(p2)
-plants.append(p3)
-
-for i in range (0, len(plants)):
-    print(plants[i].get_info())
-    print("\n")
+    for plant in plants:
+        print(plant.get_info())
+        print("\n")
