b8acbece0e0683a2081236a824924ee8b28f5da7
GayaOliveira <gmo01233210@gmail.com>
2026-02-19 19:38:07 -0300

    style: adjust the code to comply with Flake8 in ex4

--- Module01/ex4/ft_garden_security.py
+++ Module01/ex4/ft_garden_security.py
@@ -9,7 +9,7 @@ class SecurePlant:
 
     def get_height(self) -> int:
         return self.__height
-    
+
     def get_age(self) -> int:
         return self.__age
 
@@ -18,25 +18,31 @@ class SecurePlant:
 
     def set_height(self, new_height: int) -> None:
         if new_height < 0:
-            print(f"Invalid operation attempted: height {new_height}cm [REJECTED]")
-            print(f"Security: Negative height rejected")
-            print()
-            print(f"Current plant: {self.get_name()} ({self.get_height()}cm, {self.get_age()} days)")
+            to_print = "Invalid operation attempted: height"
+            to_print += f" {new_height}cm [REJECTED]\n"
+            to_print += "Security: Negative height rejected\n\n"
+            to_print += f"Current plant: {self.get_name()} "
+            to_print += f"({self.get_height()}cm, {self.get_age()} days)"
+            print(to_print)
         else:
             self.__height = new_height
-    
+
     def set_age(self, new_age: int) -> None:
         if new_age < 0:
-            print(f"Invalid operation attempted: age {new_age} days [REJECTED]")
-            print(f"Security: Negative age rejected")
-            print()
-            print(f"Current plant: {self.get_name()} ({self.get_height()}cm, {self.get_age()} days)")
+            to_print = "Invalid operation attempted: age"
+            to_print += f" {new_age}cm [REJECTED]\n"
+            to_print += "Security: Negative height rejected\n\n"
+            to_print += f"Current plant: {self.get_name()} "
+            to_print += f"({self.get_height()}cm, {self.get_age()} days)"
+            print(to_print)
         else:
-            self.__age = new_age        
+            self.__age = new_age
 
     def __str__(self) -> str:
-        return f"Plant created: {self.__name}\nHeight updated: {self.__height}cm [OK]\nAge updated: {self.__age} days [OK]"
-    
+        ret = f"Plant created: {self.__name}\nHeight updated: {self.__height}"
+        ret += f"cm [OK]\nAge updated: {self.__age} days [OK]"
+        return ret
+
 
 if __name__ == "__main__":
     rose = SecurePlant("Rose", 25, 30)
