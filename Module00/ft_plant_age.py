be5ca1fb60281a69ce95a97c2c4b16d6c84887ff
GayaOliveira <gmo01233210@gmail.com>
2026-02-20 23:18:46 -0300

    style: adjust the code to comply with Flake8 in Module0

--- Module00/ft_plant_age.py
+++ Module00/ft_plant_age.py
@@ -1,5 +1,5 @@
 def ft_plant_age():
-    plant_age = int(input(f"Enter plant age in days: "))
+    plant_age = int(input("Enter plant age in days: "))
     if plant_age > 60:
         print("Plant is ready to harvest!")
     else:
