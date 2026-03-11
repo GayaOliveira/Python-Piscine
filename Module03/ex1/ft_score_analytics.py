75e7093cec33feb0cffbda795b09f365e6f7dfaa
gmarinho <supergaya@yahoo.com>
2026-03-11 19:06:08 -0300

    feat: add ex0 to ex2

---
+++ Module03/ex1/ft_score_analytics.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def generate_number_list(args: list[str]) -> list[int]:
+    numbers: list[int] = []
+    for arg in args[1:]:
+        try:
+            num = int(arg)
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input: arguments must be integer numbers!")
+    return numbers
+
+
+def calculate_score_analytics(nums: list[int]) -> None:
+    total_players = len(nums)
+    print(f"Total players: {total_players}")
+    total_score = sum(nums)
+    print(f"Total score: {total_score}")
+    average = total_score / total_players
+    print(f"Average score: {average:.1f}")
+    high_score = max(nums)
+    print(f"High score: {high_score}")
+    low_score = min(nums)
+    print(f"Low score: {low_score}")
+    print(f"Score range: {high_score - low_score}")
+
+
+if __name__ == "__main__":
+    print("=== Player Score Analytics ===")
+    if (len(sys.argv) == 1):
+        print(
+            "No scores provided. "
+            "Usage: python3 ft_score_analytics.py <score1> <score2> ..."
+            )
+    else:
+        numbers = generate_number_list(sys.argv)
+        print(f"Scores processed: {numbers}")
+        calculate_score_analytics(numbers)
---
+++ Module03/ex2/ft_coordinate_system.py
@@ -0,0 +1,57 @@
+import math
+
+
+def calculate_distance(
+        p1: tuple[int, int, int],
+        p2: tuple[int, int, int]
+        ) -> int:
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    radicand = (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1)
+    distance = math.sqrt(float(radicand))
+    return distance
+
+
+if __name__ == "__main__":
+    print("=== Game Coordinate System ===\n")
+
+    origin = tuple((0, 0, 0))
+    created_position = tuple((10, 20, 5))
+    print(f"Position created: {created_position}")
+    print(
+        f"Distance between {origin} and {created_position}: "
+
+
+        f"{calculate_distance(origin, created_position):.2f}\n"
+        )
+
+    print("Parsing coordinates: \"3,4,0\"")
+    str_coord = "3,4,0"
+    list_coord = str_coord.split(",")
+    parsed_position = tuple(
+        [int(list_coord[0]), int(list_coord[1]), int(list_coord[2])]
+        )
+    print(f"Parsed position: {parsed_position}")
+    print(
+        f"Distance between {origin} and {parsed_position}: "
+        f"{calculate_distance(origin, parsed_position):.1f}\n"
+        )
+
+    print("Parsing invalid coordinates: \"abc,def,ghi\"")
+    str_coord2 = "abc,def,ghi"
+    list_coord2 = str_coord2.split(",")
+    try:
+        parsed_position2 = tuple(
+            [int(list_coord2[0]), int(list_coord2[1]), int(list_coord2[2])]
+            )
+    except ValueError as error:
+        print(f"Error parsing coordinates: {error}")
+        print(
+            f"Error details - Type: {error.__class__.__name__}, "
+            f"Args: {error.args}\n"
+            )
+
+    print("Unpacking demonstration:")
+    x, y, z = parsed_position
+    print(f"Player at x={x}, y={y}, z={z}")
+    print(f"Coordinates: X={x}, Y={y}, Z={z}")
