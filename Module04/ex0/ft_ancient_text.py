ea586400a3502e7ad91c9ab79382c6fad6799841
gmarinho <supergaya@yahoo.com>
2026-03-17 20:29:49 -0300

    refactor: remove 'with statement' from ex0

--- Module04/ex0/ft_ancient_text.py
+++ Module04/ex0/ft_ancient_text.py
@@ -4,31 +4,33 @@ if __name__ == "__main__":
     print("==> Accessing Storage Vault: ancient_fragment.txt")
 
     try:
-        with open("ancient_fragment.txt", "r") as file:
-            print("Connection established...\n")
-            content = file.read()
-            print("RECOVERED DATA:")
-            print(content)
-            print("\nData recovery complete.")
+        file = open("ancient_fragment.txt", "r")
+        print("Connection established...\n")
+        content = file.read()
+        print("RECOVERED DATA:")
+        print(content)
+        print("\nData recovery complete.")
     except FileNotFoundError as err:
         print(f"Error: {err.args[1]}. Impossible to established connection.")
     except Exception as err:
         print(f"An unexpected error occurred: {err.args[1]}")
     finally:
         print("Storage unit disconnected.")
+        file.close()
 
     print("\n==> Accessing Storage Vault: recent_fragment.txt")
 
     try:
-        with open("recent_fragment.txt", "r") as file:
-            print("Connection established...\n")
-            content = file.read()
-            print("RECOVERED DATA:")
-            print(content)
-            print("\nData recovery complete.")
+        file = open("recent_fragment.txt", "r")
+        print("Connection established...\n")
+        content = file.read()
+        print("RECOVERED DATA:")
+        print(content)
+        print("\nData recovery complete.")
     except FileNotFoundError as err:
         print(f"Error: {err.args[1]}. Impossible to established connection.")
     except Exception as err:
         print(f"An unexpected error occurred: {err.args[1]}")
     finally:
         print("\nStorage unit disconnected.")
+        file.close()
