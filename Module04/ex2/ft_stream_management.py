4b72e61148f857d8f0dbe6ae5b681ceb75359a0b
gmarinho <supergaya@yahoo.com>
2026-03-19 15:09:11 -0300

    refactor: change order output in ex2

--- Module04/ex2/ft_stream_management.py
+++ Module04/ex2/ft_stream_management.py
@@ -4,9 +4,9 @@ import sys
 if __name__ == "__main__":
     print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
 
-    print("Input Stream active. Enter archivist ID: ")
+    print("Input Stream active. Enter archivist ID: ", end="", flush=True)
     archivist_id = sys.stdin.readline().strip()
-    print("Input Stream active. Enter status report: ")
+    print("Input Stream active. Enter status report: ", end="", flush=True)
     status_report = sys.stdin.readline().strip()
 
     print()
