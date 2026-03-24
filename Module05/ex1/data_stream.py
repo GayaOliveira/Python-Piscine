faf8d3dab68cd80f0a15bea5be19ef63aa9f75f4
GayaOliveira <gmo01233210@gmail.com>
2026-03-23 21:00:08 -0300

    wip: continue ex1

--- Module05/ex1/data_stream.py
+++ Module05/ex1/data_stream.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Optional, Union
 
+
+class BatchError(Exception):
+    pass
+
+
 class DataStream(ABC):
 
     def is_str_list(self, data_batch: List[Any]) -> bool:
@@ -26,33 +31,20 @@ class SensorStream(DataStream):
     def __init__(self, sensor_id: str) -> None:
         super().__init__()
         self.sensor_id = sensor_id
-        self.__readings: Dict[str, float] = {}
+        self.__readings: List[List[str, float]] = []
 
-    # def __populate_dict(self, data_batch: List[str]) -> bool:
-    #     for data in data_batch:
-    #         separator_index = data.index(":")
-    #         if separator_index == -1:
-    #             return False
-    #         key = data[:separator_index]
-    #         try:
-    #             value = float(data[separator_index + 1:])
-    #             self.__readings[key] = self.__readings.get(key, 0) + value
-    #         except ValueError:
-    #             return False
-    #     return True
-    
     def __populate_dict(self, data_batch: List[str]) -> bool:
-        for data in data_batch:
-            separator_index = data.index(":")
-            if separator_index == -1:
-                return False
-            key = data[:separator_index]
-            try:
+        try:
+            for data in data_batch:
+                separator_index = data.index(":")
+                if separator_index == -1:
+                    return False
+                key = data[:separator_index]
                 value = float(data[separator_index + 1:])
-                self.__readings[key] = self.__readings.get(key, 0) + value
-            except ValueError:
-                return False
-        return True
+                self.__readings.append([key, value])
+            return True
+        except:
+            return False
 
     def count_readings(self, data_batch: List[Any]) -> int:
         if not self.is_str_list(data_batch):
@@ -67,18 +59,39 @@ class SensorStream(DataStream):
 
     def process_batch(self, data_batch: List[Any]) -> str:
         if not self.is_str_list(data_batch):
-            return "Invalid Batch..."
+            raise BatchError("Invalid Batch Data")
         if not self.__populate_dict(data_batch):
             if self.__readings:
                 self.__readings.clear()
-            return "Invalid Batch..."
+            raise BatchError("Invalid Batch Data")
         return f"{data_batch}"
 
     def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-        pass
+        if not self.__readings:
+            try:
+                self.process_batch(data_batch)
+            except Exception as err:
+                print(err)
+        filtered_list: List[Any] = []
+        for element in self.__readings:
+            if element[0] == criteria:
+                filtered_list.append(element[1])
+        return filtered_list
+    
+    def average_temp_readings(self) -> int:
+        temp_list = self.filter_data(self.__readings, "temp")
+        return sum(temp_list) / len(temp_list)       
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        return self.__readings
+        temp_list = self.filter_data(self.__readings, "temp")
+        humidity_list = self.filter_data(self.__readings, "humidity")
+        pressure_list = self.filter_data(self.__readings, "pressure")
+        sensor_dict: Dict[str, Union[float]] = {
+            "temp": temp_list,
+            "humidity": humidity_list,
+            "pressure": pressure_list
+        }
+        return sensor_dict
     
 
 class TransactionStream(DataStream):
@@ -102,30 +115,29 @@ class StreamProcessor():
     
     @staticmethod
     def convert_values_to_float(data_dict: Dict[str, Union[str, int, float]]) -> Dict[str, float]:
-        for value in data_dict.values():
-            value = float(value)
+        for value_list in data_dict.values():
+            for value in value_list:
+                value = float(value)
         return data_dict
-
-    @staticmethod
-    def calc_average_temp(data_dict: Dict[str, float], readings: int) -> float:
-        temp_sum = 0
-        for parameter, value in data_dict.items():
-            if parameter == "temp":
-                temp_sum += value
-        return temp_sum / readings
 
     def analyze_sensor_data(self, data_batch: List[str]) -> str:
         readings = self.sensor.count_readings(data_batch)
-        self.sensor.process_batch(data_batch)
-        data_dict = self.sensor.get_stats()
-        data_dict = self.convert_values_to_float(data_dict)
-        print(data_dict)
         analysis = "Sensor analysis: "
-        analysis += f"{readings} readings"
-        if len(data_dict) == 1:
-            analysis = analysis[:-1]
-        analysis += " processed, "
-        analysis += f"avg temp: {self.calc_average_temp(data_dict, readings)}°C"
+        try:
+            self.sensor.process_batch(data_batch)
+            data_dict = self.sensor.get_stats()
+            data_dict = self.convert_values_to_float(data_dict)
+            if data_dict == {}:
+                analysis += "Invalid Batch Data"
+            else:
+                avg_temp_readings = self.sensor.average_temp_readings()
+                analysis += f"{readings} readings"
+                if len(data_dict) == 1:
+                    analysis = analysis[:-1]
+                analysis += " processed, "
+                analysis += f"avg temp: {avg_temp_readings}°C"
+        except Exception as err:
+            analysis += "Invalid Batch Data"
         return analysis
 
     def analyze_stream_stats(self, multi_batch: Dict[str, List[str]]) -> None:
@@ -147,14 +159,15 @@ def main() -> None:
 
     stream_processor = StreamProcessor("SENSOR_001", "TRANS_001", "EVENT_001")
     multidata_batch = {
-        "Environmental Data": ["temp:22.5", "humidity:65", "pressure:1013", "temp: 27.5", "temp: 32"]
+        "Environmental Data": ["humidity:65", "pressure:1013", "temp: 27.5", "Gaya"]
     }
+    # ["temp:22.5", "humidity:65", "pressure:1013", "temp: 27.5", "temp: 31"]
     stream_processor.analyze_stream_stats(multidata_batch)
     # multidata_batch = {
     #     "Environmental Data": ["temp:22.5", "humidity:65", "pressure:1013"],
     #     "Financial Data": ["buy:100", "sell:150", "buy:75"],
     #     "System Events":  ["login", "error", "logout"]
-    # }
+    # } 
 
 if __name__ == "__main__":
     main()
