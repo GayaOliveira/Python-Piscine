9b9c19f85a35e00a4c54e2247485bcbcae6d88ed
GayaOliveira <gmo01233210@gmail.com>
2026-04-05 23:55:18 -0300

    wip: implement subject alterations in ex0

--- Module05/ex0/stream_processor.py
+++ Module05/ex0/stream_processor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from abc import ABC, abstractmethod
-from typing import Any, List
+from typing import Any, List, Dict
 
 
 class DataProcessor(ABC):
@@ -63,95 +63,108 @@ class TextProcessor(DataProcessor):
 
 class LogProcessor(DataProcessor):
 
-    def process(self, data: Any) -> str:
-        if (self.validate(data)):
-            separator = data.find(":")
-            log = data[:separator]
-            log_message = data[separator + 2:]
-            result = f"[ALERT] {log} level detected: {log_message}"
-            return result
-        return "Processing failed..."
+    def ingest(self, data: Dict[str, str] | List[Dict[str, str]]) -> None:
+        if data and not self.validate(data):
+            raise Exception("Improper log data")
+        elif isinstance(data, dict):
+            log_level = data['log_level']
+            log_message = data['log_message']
+            ingested = f"{log_level}: {log_message}"
+            self.processed_data.append(ingested)
+        else:
+            for dic in data:
+                log_level = dic['log_level']
+                log_message = dic['log_message']
+                ingested = f"{log_level}: {log_message}"
+                self.processed_data.append(ingested)
+
+    def is_dict_str_str(self, item: Any) -> bool:
+        if not isinstance(item, dict):
+            return False
+        mandatory_keys = {"log_level", "log_message"}
+        if set(item.keys()) != mandatory_keys:
+            return False
+        return all(
+            isinstance(k, str) and isinstance(v, str)
+            for k, v in item.items()
+        )
 
     def validate(self, data: Any) -> bool:
-        logs = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-        try:
-            separator = data.find(":")
-            log = data[:separator]
-            if log in logs:
-                return True
-            return False
-        except Exception:
-            return False
+        if isinstance(data, dict):
+            return self.is_dict_str_str(data)
+        
+        if isinstance(data, list) and data:
+            return all(self.is_dict_str_str(item) for item in data)
+        
+        return False
 
 
 def main() -> None:
 
-    print("\n==> Testing Numeric Processor...")
+    print("\nTesting Numeric Processor...")
     num_proces = NumericProcessor()
-    print(f"Trying to validate input '42': {num_proces.validate(42)}")
-    print(f"Trying to validate input 'Hello': {num_proces.validate("Hello")}")
-    print(f"Trying to validate input '(4.5, -2)': {num_proces.validate((4.5, -2))}")
-    print(f"Trying to validate input '[4.5, -2]': {num_proces.validate([4.5, -2])}")
-    print(f"Trying to validate input 'None': {num_proces.validate(None)}")
-    print("Test invalid ingestion of string 'foo' without prior validation:")
+    print(f"  Trying to validate input '42': {num_proces.validate(42)}")
+    print(f"  Trying to validate input 'Hello': {num_proces.validate("Hello")}")
+    print(f"  Trying to validate input '(4.5, -2)': {num_proces.validate((4.5, -2))}")
+    print(f"  Trying to validate input '[4.5, -2]': {num_proces.validate([4.5, -2])}")
+    print(f"  Trying to validate input 'None': {num_proces.validate(None)}")
+    print("  Test invalid ingestion of string 'foo' without prior validation:")
     try:
         num_proces.ingest("foo")
     except Exception as err:
-        print(f"Got exception: {err}")
+        print(f"  Got exception: {err}")
     data1 = [1, 2, 3, 4, 5]
-    print(f"Processing data: {data1}")
+    print(f"  Processing data: {data1}")
     num_proces.ingest(data1)
     times = 3
-    print(f"Extracting {times} {'value' if times == 1 else 'values'}...")
+    print(f"  Extracting {times} {'value' if times == 1 else 'values'}...")
     for _ in range(times):
         index, element = num_proces.output()
-        print(f"Numeric value {index}: {element}")
+        print(f"  Numeric value {index}: {element}")
     print()
 
-    print("==> Testing Text Processor...")
+    print("Testing Text Processor...")
     text_proces = TextProcessor()
-    print(f"Trying to validate input '42': {text_proces.validate(42)}")
-    print(f"Trying to validate input 'Hello': {text_proces.validate("Hello")}")
-    print(f"Trying to validate input '('Hello', '42')': {text_proces.validate(("Hello", "42"))}")
-    print(f"Trying to validate input '['Hello', '42']': {text_proces.validate(["Hello", "42"])}")
-    print(f"Trying to validate input 'None': {text_proces.validate(None)}")
-    print("Test invalid ingestion of number 500 without prior validation:")
+    print(f"  Trying to validate input '42': {text_proces.validate(42)}")
+    print(f"  Trying to validate input 'Hello': {text_proces.validate("Hello")}")
+    print(f"  Trying to validate input '('Hello', '42')': {text_proces.validate(("Hello", "42"))}")
+    print(f"  Trying to validate input '['Hello', '42']': {text_proces.validate(["Hello", "42"])}")
+    print(f"  Trying to validate input 'None': {text_proces.validate(None)}")
+    print("  Test invalid ingestion of number 500 without prior validation:")
     try:
         text_proces.ingest(500)
     except Exception as err:
-        print(f"Got exception: {err}")
+        print(f"  Got exception: {err}")
     data2 = ['Hello', 'Nexus', 'World']
-    print(f"Processing data: {data2}")
+    print(f"  Processing data: {data2}")
     text_proces.ingest(data2)
     times = 2
-    print(f"Extracting {times} {'value' if times == 1 else 'values'}...")
+    print(f"  Extracting {times} {'value' if times == 1 else 'values'}...")
     for _ in range(times):
         index, element = text_proces.output()
-        print(f"Text value {index}: {element}")
+        print(f"  Text value {index}: {element}")
     print()
 
-    
-
-    
-    # print("\n\n==> Initializing Log Processor...\n")
-    # log_processor = LogProcessor()
-    # log_tests: List[Any] = [
-    #     "INFO: File loaded successfully",
-    #     "WARNING: Disk space running low",
-    #     "ERROR: Connection timeout",
-    #     "CRITICAL: System out of memory",
-    #     "log",
-    #     ["log1", "log2"]
-    # ]
-    # for test in log_tests:
-    #     print(f"Processing data: {test}")
-    #     output = log_processor.process(test)
-    #     if output == "Processing failed...":
-    #         print("Validation fail: Not log data")
-    #     else:
-    #         print("Validation: Log data verified")
-    #     print((f"Output: {log_processor.format_output(output)}"))
-    #     print("--------------------------------------")
+    print("Testing Log Processor...")
+    log_proces = LogProcessor()
+    print(f"  Trying to validate input 'Hello': {log_proces.validate("Hello")}")
+    print("  Test invalid ingestion of dict without prior validation:")
+    try:
+        log_proces.ingest({'log_level': 'NOTICE'})
+    except Exception as err:
+        print(f"  Got exception: {err}")
+    data3 = [
+        {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+    ]
+    print(f"  Processing data: {data3}")
+    log_proces.ingest(data3)
+    times = 2
+    print(f"  Extracting {times} {'value' if times == 1 else 'values'}...")
+    for _ in range(times):
+        index, element = log_proces.output()
+        print(f"  Log entry {index}: {element}")
+    print()
 
 
 if __name__ == "__main__":
