d91a9dd854ee0a62289fa855c5a728b68c2701ae
GayaOliveira <gmo01233210@gmail.com>
2026-04-06 08:16:28 -0300

    fix: correct logic in ingest methods and correct Flake8 issues in ex0

--- Module05/ex0/stream_processor.py
+++ Module05/ex0/stream_processor.py
@@ -24,7 +24,7 @@ class DataProcessor(ABC):
         return self.index, data
 
 
-class NumericProcessor(DataProcessor):  
+class NumericProcessor(DataProcessor):
 
     def validate(self, data: Any) -> bool:
         if isinstance(data, (int, float)):
@@ -32,9 +32,9 @@ class NumericProcessor(DataProcessor):
         if isinstance(data, list) and data:
             return all(isinstance(item, (int, float)) for item in data)
         return False
-    
+
     def ingest(self, data: int | float | List[int | float]) -> None:
-        if data and not self.validate(data):
+        if data is not None and not self.validate(data):
             raise Exception("Improper numeric data")
         elif isinstance(data, (int, float)):
             self.processed_data.append(str(data))
@@ -53,7 +53,7 @@ class TextProcessor(DataProcessor):
         return False
 
     def ingest(self, data: str | List[str]) -> None:
-        if data and not self.validate(data):
+        if data is not None and not self.validate(data):
             raise Exception("Improper text data")
         elif isinstance(data, (str)):
             self.processed_data.append(data)
@@ -62,21 +62,6 @@ class TextProcessor(DataProcessor):
 
 
 class LogProcessor(DataProcessor):
-
-    def ingest(self, data: Dict[str, str] | List[Dict[str, str]]) -> None:
-        if data and not self.validate(data):
-            raise Exception("Improper log data")
-        elif isinstance(data, dict):
-            log_level = data['log_level']
-            log_message = data['log_message']
-            ingested = f"{log_level}: {log_message}"
-            self.processed_data.append(ingested)
-        else:
-            for dic in data:
-                log_level = dic['log_level']
-                log_message = dic['log_message']
-                ingested = f"{log_level}: {log_message}"
-                self.processed_data.append(ingested)
 
     def is_dict_str_str(self, item: Any) -> bool:
         if not isinstance(item, dict):
@@ -92,11 +77,26 @@ class LogProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         if isinstance(data, dict):
             return self.is_dict_str_str(data)
-        
+
         if isinstance(data, list) and data:
             return all(self.is_dict_str_str(item) for item in data)
-        
+
         return False
+
+    def ingest(self, data: Dict[str, str] | List[Dict[str, str]]) -> None:
+        if data is not None and not self.validate(data):
+            raise Exception("Improper log data")
+        elif isinstance(data, dict):
+            log_level = data['log_level']
+            log_message = data['log_message']
+            ingested = f"{log_level}: {log_message}"
+            self.processed_data.append(ingested)
+        else:
+            for dic in data:
+                log_level = dic['log_level']
+                log_message = dic['log_message']
+                ingested = f"{log_level}: {log_message}"
+                self.processed_data.append(ingested)
 
 
 def main() -> None:
@@ -104,18 +104,22 @@ def main() -> None:
     print("\nTesting Numeric Processor...")
     num_proces = NumericProcessor()
     print(f"  Trying to validate input '42': {num_proces.validate(42)}")
-    print(f"  Trying to validate input 'Hello': {num_proces.validate("Hello")}")
-    print(f"  Trying to validate input '(4.5, -2)': {num_proces.validate((4.5, -2))}")
-    print(f"  Trying to validate input '[4.5, -2]': {num_proces.validate([4.5, -2])}")
-    print(f"  Trying to validate input 'None': {num_proces.validate(None)}")
+    print(f"  Trying to validate input 'Hello': "
+          f"{num_proces.validate('Hello')}")
+    print(f"  Trying to validate input '(4.5, -2)': "
+          f"{num_proces.validate((4.5, -2))}")
+    print(f"  Trying to validate input '[4.5, -2]': "
+          f"{num_proces.validate([4.5, -2])}")
+    print(f"  Trying to validate input 'None': "
+          f"{num_proces.validate(None)}")
     print("  Test invalid ingestion of string 'foo' without prior validation:")
     try:
-        num_proces.ingest("foo")
+        num_proces.ingest("foo")   # type: ignore[arg-type]
     except Exception as err:
         print(f"  Got exception: {err}")
     data1 = [1, 2, 3, 4, 5]
     print(f"  Processing data: {data1}")
-    num_proces.ingest(data1)
+    num_proces.ingest(data1)    
     times = 3
     print(f"  Extracting {times} {'value' if times == 1 else 'values'}...")
     for _ in range(times):
@@ -126,13 +130,16 @@ def main() -> None:
     print("Testing Text Processor...")
     text_proces = TextProcessor()
     print(f"  Trying to validate input '42': {text_proces.validate(42)}")
-    print(f"  Trying to validate input 'Hello': {text_proces.validate("Hello")}")
-    print(f"  Trying to validate input '('Hello', '42')': {text_proces.validate(("Hello", "42"))}")
-    print(f"  Trying to validate input '['Hello', '42']': {text_proces.validate(["Hello", "42"])}")
+    print(f"  Trying to validate input 'Hello': "
+          f"{text_proces.validate('Hello')}")
+    print(f"  Trying to validate input '('Hello', '42')': "
+          f"{text_proces.validate(('Hello', '42'))}")
+    print(f"  Trying to validate input '['Hello', '42']': "
+          f"{text_proces.validate(['Hello', '42'])}")
     print(f"  Trying to validate input 'None': {text_proces.validate(None)}")
     print("  Test invalid ingestion of number 500 without prior validation:")
     try:
-        text_proces.ingest(500)
+        text_proces.ingest(500)    # type: ignore[arg-type]
     except Exception as err:
         print(f"  Got exception: {err}")
     data2 = ['Hello', 'Nexus', 'World']
@@ -147,7 +154,8 @@ def main() -> None:
 
     print("Testing Log Processor...")
     log_proces = LogProcessor()
-    print(f"  Trying to validate input 'Hello': {log_proces.validate("Hello")}")
+    print(f"  Trying to validate input 'Hello': "
+          f"{log_proces.validate("Hello")}")
     print("  Test invalid ingestion of dict without prior validation:")
     try:
         log_proces.ingest({'log_level': 'NOTICE'})
